59dae65ce0273b10ff17047dbf6caa1c62fe4a18
pixiewebsolution-maker <pixiewebsolution@gmail.com>
2026-06-14 05:24:55 +0530

    chore: completely remove sentry following uninstallation

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  silent: !process.env.CI,
-  telemetry: false, // Disables telemetry warnings
-  sourcemaps: {
-    disable: true,
-  },
-  tunnelRoute: "/monitoring",
-});
+export default nextConfig;
